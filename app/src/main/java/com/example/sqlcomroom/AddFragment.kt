16491dc95d0055cc545190e853177ae9e67cd0dc
Marih Souza <marina-queiroz2011@hotmail.com>
2022-04-01 14:42:59 -0300

    att sql room

--- app/src/main/java/com/example/sqlcomroom/AddFragment.kt
+++ app/src/main/java/com/example/sqlcomroom/AddFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.example.sqlcomroom.data.User
 import com.example.sqlcomroom.databinding.FragmentAddBinding
 
 class AddFragment : Fragment() {
+    private lateinit var mainViewModel:MainViewModel
 
     private lateinit var binding: FragmentAddBinding
 
@@ -20,5 +24,23 @@ class AddFragment : Fragment() {
             layoutInflater, container, false
         )
         return binding.root
+    }
+    fun validar (nome: String, sobrenome: String, idade: String): Boolean{
+        return !(nome.isEmpty() || sobrenome.isEmpty() || idade.isEmpty())
+    }
+
+    fun inserirNoBanco(){
+        val nome = binding.editNome.text.toString()
+        val sobrenome = binding.editSobreNome.text.toString()
+        val idade = binding.editTextNumber.text.toString()
+
+        if(validar(nome, sobrenome, idade)){
+            val user = User(0,nome,sobrenome, idade.toInt())
+            mainViewModel.addUser(user)
+            Toast.makeText(
+                context, "Usuário Cadastrado", Toast.LENGTH_SHORT
+            ).show()
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+        }
     }
 }
--- app/src/main/java/com/example/sqlcomroom/ListFragment.kt
+++ app/src/main/java/com/example/sqlcomroom/ListFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.sqlcomroom.adapter.UserAdapter
 import com.example.sqlcomroom.databinding.FragmentListBinding
@@ -12,7 +13,7 @@ import com.example.sqlcomroom.databinding.FragmentListBinding
 class ListFragment : Fragment() {
 
     private lateinit var binding: FragmentListBinding
-
+    private lateinit var mainViewModel:MainViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,6 +28,18 @@ class ListFragment : Fragment() {
         binding.recyclerUser.layoutManager = LinearLayoutManager(context)
         binding.recyclerUser.adapter = adapter
         binding.recyclerUser.setHasFixedSize(true)
+
+        binding.floatingAdd.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        mainViewModel = MainViewModel(context)
+
+        mainViewModel.lerTodosOsDados.observe(viewLifecycleOwner,{
+                response -> if (response != null){
+            adapter.setList(response)
+        }
+            })
 
         return binding.root
     }
